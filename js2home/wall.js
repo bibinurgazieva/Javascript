c7508c3fedb7ab9c4ea614d5087db9f040ec1a27
bibinurgazieva <you@example.com>
2021-12-10 17:09:48 +0600

    ...

--- js2home/wall.js
+++ js2home/wall.js
@@ -1,4 +1,4 @@
 let length = Number(prompt("Please enter the length of your wall in meters"))
 let width = Number(prompt("Please enter the width of your wall in meters"))
 let s = Number(length)*Number(width)
-console.log String("The area of your wall is" + s)
+console.log(String("The area of your wall is" + s))
