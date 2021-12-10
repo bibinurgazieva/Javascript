7e3f552535ad0a091f3dd34e42c8425a1440bc37
bibinurgazieva <you@example.com>
2021-12-10 10:50:59 +0600

    new theme

---
+++ js2/js2.js
@@ -0,0 +1,17 @@
+//  let myName = prompt()
+//  console.log(myName)
+
+// let year = prompt("Введите год вашего рождения:")
+// let age = 2021 - Number(year)
+// console.log("Вам " + String(age) + " лет")
+
+// вводите стороны прямоугольника,
+// надо найти и вывести периметр
+// let a = Number(prompt("Side 1:"))
+// let b = Number(prompt("Side 2:"))
+// let p = 2*(Number(a)+Number(b))
+// console.log(p)
+
+let r = Number(prompt("Radius of circle"))
+let s = 3.14 * (r ** 2)
+console.log(s)
