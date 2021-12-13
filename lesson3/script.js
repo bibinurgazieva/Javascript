88972f34f0b7b041cf18c87735a43049f8e9f531
bibinurgazieva <you@example.com>
2021-12-13 17:11:33 +0600

    changed login admin

--- lesson3/script.js
+++ lesson3/script.js
@@ -8,15 +8,32 @@
 //     console.log("welcome!")
 // }
 
-let age = Number(prompt("Enter your age"))
-if (age <= 12) {
-    console.log("You are a kid")
-} else if(age <= 18) {
-    console.log("teenager")
-} else if( age <= 23) {
-    console.log("student")
-} else if (age >= 24) {
-    console.log("adult")
-} else if (age >= 65) {
-    console.log("pensioneer")
+// let age = Number(prompt("Enter your age"))
+// if (age <= 12) {
+//     console.log("You are a kid")
+// } else if(age <= 18) {
+//     console.log("teenager")
+// } else if( age <= 23) {
+//     console.log("student")
+// } else if (age <= 65) {
+//     console.log("adult")
+// }
+//   else if (age == 100) {
+//         console.log("You've got a present")
+// } else if (age >= 65) {
+//     console.log("pensioneer")
+// } 
+// &&
+// ||
+
+
+// const a = Number(prompt())
+// if (a > 5 && a < 10) {
+//     console.log("larger than 5 and less than 10")
+// }
+const login = String(prompt("login"))
+if (login == "admin" || login == "manager") {
+    console.log("hello")
+} else {
+    console.log("no access")
 }
