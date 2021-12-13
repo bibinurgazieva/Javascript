e7ea7bfb3fa0c031cabd01f01db36cb04358499a
bibinurgazieva <you@example.com>
2021-12-13 17:44:26 +0600

    lesson

---
+++ lesson4/script.js
@@ -0,0 +1,11 @@
+// let b = true
+// "be or not to be"
+// console.log(b || !b)
+let s = Number(prompt("Area of room"))
+
+let n = 1
+if (s > 30) {
+    n = 4
+}
+
+console.log("you need " + String(n) + " lightning")
