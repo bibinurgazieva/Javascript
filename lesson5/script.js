3d10075953b0afd5387c88693ab75b60f03070e4
bibinurgazieva <you@example.com>
2021-12-15 20:37:07 +0600

    indexes array

---
+++ lesson5/script.js
@@ -0,0 +1,25 @@
+let myArray = [5, "hi", 7, false, null, 0]
+console.log(myArray[1])
+
+let m = "ultra"
+
+let myObject = {
+    "ball": [8, 72, 98],
+    7: -5,
+    "start": true,
+    "void": undefined,
+    700: m
+}
+console.log(myObject[2])
+console.log(myObject["start"])
+console.log(myObject[700])
+console.log(myObject.ball[1])
+
+let desk = {
+    color: "red",
+    height: 1.5,
+    markers: null,
+
+}
+console.log(desk["color"])
+console.log(desk.color)
