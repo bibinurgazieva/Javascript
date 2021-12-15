f694adbd73ade91aabd986a7f71c0ec27efaad04
bibinurgazieva <you@example.com>
2021-12-15 16:40:51 +0600

    chenges

--- lesson4/script.js
+++ lesson4/script.js
@@ -1,11 +1,50 @@
 // let b = true
 // "be or not to be"
 // console.log(b || !b)
-let s = Number(prompt("Area of room"))
+// let s = Number(prompt("Area of room"))
 
-let n = 1
-if (s > 30) {
-    n = 4
-}
+// let n = 0
+// let error = false 
+// if (s > 30 && s < 50) {
+//     n = 4
+// } else if (s > 50 && s < 150) {
+//     n = 8
+// } else if (s >= 150) {
+//     n = 10
+// } else if (s > 0 && s <= 30) {
+//     n=1
+// } else if (s <= 0) {
+//     console.log("fatality")
+//     error = true
+// }
+// let lamp = "lampochek"
+// if (n == 1) {
+//     lamp = "lampochka"
+// } else if (n == 2 || 4) {
+//     lamp = "lampochki"
+// } 
+// if (!error) {
+//     console.log("you need " + String(n) + lamp)
+// }
 
-console.log("you need " + String(n) + " lightning")
+// let n = 1
+// if (s > 30) {
+//     n = 4
+// }
+// let a = "hello world!"
+
+// console.log(a[4]) 
+
+// let b = a.length
+// let c = b - 1
+// console.log(a[c])
+
+
+let ar = [5, 2, 7, "hello", 3.14]
+console.log(ar[2])
+console.log(ar.length)
+ar.push(123)
+ar.push("world")
+console.log(ar)
+ar.pop()
+console.log(ar)
