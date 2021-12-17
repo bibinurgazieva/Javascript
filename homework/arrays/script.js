c384482cf23766f138e53efc4eea72328848568c
bibinurgazieva <you@example.com>
2021-12-17 23:07:09 +0600

    done the homework and checked

---
+++ homework/arrays/script.js
@@ -0,0 +1,56 @@
+let numbers = []
+
+let a = Number(prompt("Enter the first number"))
+let b = Number(prompt("enter the second number"))
+let c = Number(prompt("enter the second number"))
+let d = Number(prompt("enter the fourth number"))
+let e = Number(prompt("enter the 5th number"))
+let f = Number(prompt("enter the 6th number"))
+let g = Number(prompt("enter the 7th number"))
+
+
+if (a %2 === 0) {
+    numbers.push(a)
+} 
+if ( b %2 === 0) {
+    numbers.push(b)
+}
+if ( c %2 === 0) {
+    numbers.push(c)
+}
+if ( d %2 === 0) {
+    numbers.push(d)
+}
+if ( e %2 === 0) {
+    numbers.push(e)
+}
+if ( f %2 === 0) {
+    numbers.push(f)
+}
+if ( g %2 === 0) {
+    numbers.push(g)
+}
+console.log(numbers)
+
+if ( a %3 === 0) {
+    numbers.push(a)
+}
+if ( b %3 === 0) {
+    numbers.push(b)
+}
+if ( c %3 === 0) {
+    numbers.push(c)
+}
+if ( d %3 === 0) {
+    numbers.push(d)
+}
+if ( e %3 === 0) {
+    numbers.push(e)
+}
+if ( f %3 === 0) {
+    numbers.push(f)
+}
+if ( g %3 === 0) {
+    numbers.push(g)
+}
+console.log(numbers)
