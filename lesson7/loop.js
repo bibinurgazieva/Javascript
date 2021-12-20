cdfa56d128b3f39a86b0b94d84ff56913513dc60
bibinurgazieva <you@example.com>
2021-12-20 18:54:29 +0600

    massive arrays for let

---
+++ lesson7/loop.js
@@ -0,0 +1,15 @@
+let a = [5, 7, 3, 2, 8, 0, 2 ]
+
+// console.log(a[0])
+// console.log(a[1])
+// console.log(a[2])
+// console.log(a[3])
+// console.log(a[4])
+// console.log(a[5])
+// console.log(a[6])
+
+for (let i = 0; i < 7; i++) {
+    if (a[i] %2 ===0 ){
+        console.log(a[i])
+    }
+} 
---
+++ lesson7/script.js
@@ -0,0 +1,36 @@
+// let a = [6, "hello", null, false,[7, 23, true], undefined]
+// console.log(a[5]) 
+// length begins with 1, order of massive begins with 0 zero 
+// let m = [
+//     [7, 8, 5, 4]
+//     [13, 0, 5, 9]
+//     [6, 1, 15, 3]
+// ]
+// console.log(m[1] [3])
+
+let c = {
+    one: 452,
+    "two": false,
+    myArray: ["hello", "world", "js"],
+    another: {
+        7: 8,
+        "test": true,
+        vivi: null,
+        hi: [52, 73,{foo: "bar","hello": [ [62, 24, 52], [62, 24, 52], [62, 0, 522]] }]
+    }
+}
+
+// console.log(c.one)
+// console.log(c["two"])
+// console.log(c["one"])
+// console.log(c.myArray[1])
+// console.log(c.another["test"])
+// console.log(c.another.test)
+// // console.log(c.another[1]) it doesnt work because its not massive, its an object
+// c.test2 = "mosya mosya"
+// c["test 3"] = "erkin aka"
+// console.log(c)
+
+
+// console.log(c.another.hi ["hello"] [2] [1])
+console.log(c.another.hi [2] ["hello"] [2] [1])
