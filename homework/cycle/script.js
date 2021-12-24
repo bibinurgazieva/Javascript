c2edf3706edd9964151732913578401278bdbf45
bibinurgazieva <you@example.com>
2021-12-24 17:00:48 +0600

    solved

---
+++ homework/cycle/script.js
@@ -0,0 +1,16 @@
+let a = [];
+let b = [];
+
+for(i=0; i<101; i++) {
+    if( i%3 === 0){
+        a.push(i)
+    } else (i%4 ===0)
+        b.push(i)
+    }
+
+console.log(a)
+console.log(b)
+
+
+
+
