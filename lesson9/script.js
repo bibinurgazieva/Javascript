bf057f42e5e14dbed687adfe8a56e66ee88044af
bibinurgazieva <you@example.com>
2021-12-27 17:50:05 +0600

    cycle while

---
+++ lesson9/script.js
@@ -0,0 +1,28 @@
+// let m = []
+
+// for ( let i = 0; i<3; i++) {
+//     let a = prompt()
+//     m.push(a)
+// }
+// console.log(m)
+
+let n = []
+let b = prompt()
+
+while(b != "stop") {
+
+    n.push(b)
+    b = prompt()
+}
+console.log(n) 
+
+// let m = []
+// let a = Number(prompt())
+
+// while(a !==0) {
+//     if( a%7===0 ){
+//         m.push(a)
+//     }
+//     a = Number(prompt())
+// }
+// console.log(m)
