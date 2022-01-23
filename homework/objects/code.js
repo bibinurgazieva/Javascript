adf1cf1cc0d487041a689fa11395cf9b1f6d705f
bibinurgazieva <tulipus11@gmail.com>
2022-01-23 14:52:06 +0600

    hw

---
+++ homework/objects/code.js
@@ -0,0 +1,54 @@
+// // 1st ex
+
+// let user = {}
+// user.name = "bibi"
+// user.surname = "nurgazieva"
+// user.name = "mosya"
+// delete user.name 
+
+// // 3rd ex
+
+// const user = {
+//     name: "John"
+//   };
+  
+//   // это будет работать?
+//   user.name = "Pete";
+// //   yes it will work
+
+// // 4th ex
+// let salaries = {
+//     John: 100,
+//     Ann: 160,
+//     Pete: 130
+//   }
+
+// let sum =  salaries.John + salaries.Ann + salaries.Pete
+
+// console.log (sum)
+
+// // 2
+// sum = 0
+// for (let key in salaries) {
+//     sum += salaries 
+// }
+// console.log (sum)
+
+// 5th ex
+
+// let menu = {
+//     width: 200,
+//     height: 300,
+//     title: "My menu"
+//   }
+
+// function multiply (obj) {
+    
+//     for (let key in obj) {
+//         if (typeof obj [key] === 'number') {
+//             obj[key] * 2
+//         }
+
+//     }
+
+// }
