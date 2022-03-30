9dbf02fa91c5e87f3a67291ba60d1309fb6b3e1a
bibinurgazieva <tulipus11@gmail.com>
2022-03-30 08:47:36 +0600

    redone arrays

---
+++ js/homework/arrays/script.js
@@ -0,0 +1,57 @@
+let numbers2 = []
+let numbers3 = []
+
+let a = Number(prompt("Enter the first number"))
+let b = Number(prompt("enter the second number"))
+let c = Number(prompt("enter the second number"))
+let d = Number(prompt("enter the fourth number"))
+let e = Number(prompt("enter the 5th number"))
+let f = Number(prompt("enter the 6th number"))
+let g = Number(prompt("enter the 7th number"))
+
+
+if (a %2 === 0) {
+    numbers2.push(a)
+} 
+if ( b %2 === 0) {
+    numbers2.push(b)
+}
+if ( c %2 === 0) {
+    numbers2.push(c)
+}
+if ( d %2 === 0) {
+    numbers2.push(d)
+}
+if ( e %2 === 0) {
+    numbers2.push(e)
+}
+if ( f %2 === 0) {
+    numbers2.push(f)
+}
+if ( g %2 === 0) {
+    numbers2.push(g)
+}
+console.log(numbers2)
+
+if ( a %3 === 0) {
+    numbers3.push(a)
+}
+if ( b %3 === 0) {
+    numbers3.push(b)
+}
+if ( c %3 === 0) {
+    numbers3.push(c)
+}
+if ( d %3 === 0) {
+    numbers3.push(d)
+}
+if ( e %3 === 0) {
+    numbers3.push(e)
+}
+if ( f %3 === 0) {
+    numbers3.push(f)
+}
+if ( g %3 === 0) {
+    numbers3.push(g)
+}
+console.log(numbers3)
